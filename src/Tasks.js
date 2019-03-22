28a99df2238e821ffca5596e995440fed082bf4d
RaunG <raung@icloud.com>
2019-03-21 19:31:01 -0500

    all files

--- src/Tasks.js
+++ src/Tasks.js
@@ -1,32 +1,125 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 import uuid from 'uuid/v4';
+
+const initialTasksState = {
+  tasks: [],
+  completedTasks: []
+};
+
+const TYPES = {
+  ADD_TASK: 'ADD_TASK',
+  COMPLETE_TASK: 'COMPLETE_TASK',
+  DELETE_TASK: 'DELETE_TASK'
+};
+
+const tasksReducer = (state, action) => {
+  console.log('state', state, 'action', action);
+
+  switch (action.type) {
+    case TYPES.ADD_TASK:
+      return {
+        ...state,
+        tasks: [...state.tasks, action.task]
+      };
+    case TYPES.COMPLETE_TASK:
+      const { completedTask } = action;
+      return {
+        ...state,
+        completedTasks: [...state.completedTasks, completedTask],
+        tasks: state.tasks.filter(t => t.id !== completedTask.id)
+      };
+    case TYPES.DELETE_TASK:
+      return {
+        ...state,
+        completedTasks: state.completedTasks.filter(
+          t => t.id !== action.task.id
+        )
+      };
+    default:
+      return state;
+  }
+};
+
+const TASK_STORAGE_KEY = 'TASKS_STORAGE_KEY';
+
+const storeTasks = taskMap => {
+  localStorage.setItem(TASK_STORAGE_KEY, JSON.stringify(taskMap));
+};
+
+const readStoredTasks = () => {
+  const tasksMap = JSON.parse(localStorage.getItem(TASK_STORAGE_KEY));
+  return tasksMap ? tasksMap : initialTasksState;
+};
 
 function Tasks() {
   const [taskText, setTaskText] = useState('');
-  const [tasks, setTasks] = useState([]);
-  const [completedTasks, setCompletedTasks] = useState([]);
+  const storedTasks = readStoredTasks();
+
+  const [state, dispatch] = useReducer(tasksReducer, storedTasks);
+  const { tasks, completedTasks } = state;
+
+  useEffect(() => {
+    storeTasks({ tasks, completedTasks });
+  });
 
   const updateTaskText = event => {
     setTaskText(event.target.value);
   };
 
   const addTask = () => {
-    setTasks([...tasks, { taskText, id: uuid() }]);
+    dispatch({ type: TYPES.ADD_TASK, task: { taskText, id: uuid() } });
+
+    setTaskText('');
   };
 
-  console.log('tasks', tasks);
+  const completeTask = completedTask => () => {
+    dispatch({ type: TYPES.COMPLETE_TASK, completedTask });
+  };
+  const handleKeyPress = event => {
+    if (event.key === 'Enter') {
+      addTask();
+      setTaskText('');
+    }
+  };
+
+  const deleteTask = task => () => {
+    dispatch({ type: TYPES.DELETE_TASK, task });
+  };
 
   return (
     <div>
       <h3>Tasks</h3>
       <div className="form">
-        <input value={taskText} onChange={updateTaskText} />
+        <input
+          value={taskText}
+          onChange={updateTaskText}
+          onKeyPress={handleKeyPress}
+        />
         <button onClick={addTask}>Add Task</button>
       </div>
       <div className="task-list">
         {tasks.map(task => {
           const { id, taskText } = task;
-          return <div key={id}>{taskText}</div>;
+          return (
+            <div key={id} onClick={completeTask(task)}>
+              {taskText}
+            </div>
+          );
+        })}
+      </div>
+      <div className="completed-list">
+        {completedTasks.map(task => {
+          const { id, taskText } = task;
+
+          return (
+            <div key={id}>
+              {taskText}
+              {'  '}
+              <span onClick={deleteTask(task)} className="delete-task">
+                x
+              </span>
+            </div>
+          );
         })}
       </div>
     </div>
--- src/hooks.js
+++ src/hooks.js
@@ -1,7 +1,4 @@
-import {
-  useState,
-  useEffect
-} from 'react';
+import { useState, useEffect } from 'react';
 
 export const useFetch = (url, initialValue) => {
   const [result, setResult] = useState(initialValue);
@@ -12,4 +9,19 @@ export const useFetch = (url, initialValue) => {
       .then(json => setResult(json));
   }, []);
   return result;
+};
+
+export const useDynamicTransition = ({ increment, delay, length }) => {
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIndex(storedIndex => {
+        return (storedIndex + increment) % length;
+      });
+    }, delay);
+    return () => clearInterval(interval);
+  }, [delay, increment]);
+
+  return index;
 };
